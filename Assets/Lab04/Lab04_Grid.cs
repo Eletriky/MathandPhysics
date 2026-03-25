0e07a79fedacebb3e6a27da4893772184e95b60e
Eletriky <kylecowsert15@gmail.com>
2026-03-25 13:08:32 -0500

    A

--- Assets/Lab04/Lab04_Grid.cs
+++ Assets/Lab04/Lab04_Grid.cs
@@ -5,24 +5,21 @@ public class Lab04_Grid : DrawableGrid
     public override void SetupScenes()
     {
         int sceneIndex;
-        DrawableArrow newArrow;
         DrawableObject newGraph;
 
         sceneIndex = AddScene("Diamond");
         newGraph = new DrawableDiamond();
         AddObjectToScene(sceneIndex, newGraph);
 
-
         sceneIndex = AddScene("Diamond scale 20");
         newGraph = new DrawableDiamond();
-        AddObjectToScene(sceneIndex, newGraph);
         newGraph.Scale = (Vector3.one * 20);
+        AddObjectToScene(sceneIndex, newGraph);
 
         sceneIndex = AddScene("Diamond scale 20, 10");
         newGraph = new DrawableDiamond();
-        AddObjectToScene(sceneIndex, newGraph);
         newGraph.Scale = new Vector3(20, 10, 1);
-        
+        AddObjectToScene(sceneIndex, newGraph);
 
         sceneIndex = AddScene("Diamond scale 20, 10. rotation 45");
         newGraph = new DrawableDiamond();
