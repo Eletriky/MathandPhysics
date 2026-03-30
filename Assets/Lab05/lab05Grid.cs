2b239f85253d0857cd5d03b6bf580acb611d1b5d
Eletriky <kylecowsert15@gmail.com>
2026-03-30 13:33:19 -0500

    A

---
+++ Assets/Lab05/lab05Grid.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+using Rect = System.Drawing.Rectangle; 
+
+public class lab05Grid : DrawableGrid
+{
+
+    Rect box;
+    Rect boxOnGrid;
+
+    Line circleRadiusLine;
+    float circleRadius = 10;
+    Line ellipseRadiusLine;
+    Vector2 ellipseAxis = new Vector2(10, 20);
+
+    float offset;
+
+    public override void SetupScenes()
+    {
+        int sceneIndex;
+        DrawableObject newGraph;
+
+        sceneIndex = AddScene("Drawing Tools Test");
+
+
+        box = new Rect(100, 100, 100, 100);
+        boxOnGrid = new Rect(2, 3, 10, 10);
+
+
+        offset = 0;
+
+        circleRadiusLine = new Line(Vector3.zero,Vector2.zero, Color.cyan);
+        ellipseRadiusLine = new Line(Vector3.zero, Vector2.zero, Color.magenta);
+    }
+
+    public override void Tick()
+    {
+        offset += Time.deltaTime;
+        DrawingTools.DrawRectangle(box, Color.red);
+        DrawingTools.DrawRectangle(boxOnGrid, Color.green, this);
+
+        circleRadiusLine.end = DrawingTools.CircleRadiusPoint(Vector3.zero, offset * 2, circleRadius);
+        DrawLine(circleRadiusLine);
+    }
+}
